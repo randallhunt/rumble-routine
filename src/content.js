5e7976bb209e812d477e092fa84811d4fe3365f3
Randy Hunt <randy@rhunt.com>
2024-11-11 19:39:59 -0500

    add time nudge inputs

--- src/content.js
+++ src/content.js
@@ -1,4 +1,15 @@
-const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const RumbleRoutineDayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+var RumbleRoutineMouseEl = null
+var RumbleRoutineMouseStart = null
+var RumbleRoutineStartVal = null
+
+// time adding function
+function RumbleRoutineTimeAdd(val, increment, pivot) {
+  let vv = parseInt(val) + (increment % pivot)
+  if (vv < 0) vv = parseInt(pivot) + vv
+  return vv % pivot
+}
 
 // add to schedule
 async function RubmleRoutineAddToSchedule () {
@@ -11,7 +22,7 @@ async function RubmleRoutineAddToSchedule () {
   })
   let confirmed = true
   const newSchedule = { sun: [], mon: [], tue: [], wed: [], thu: [], fri: [], sat: [] }
-  dayNames.forEach(dayName => {
+  RumbleRoutineDayNames.forEach(dayName => {
     if (!confirmed) return
     const day = dayName.toLowerCase().substring(0, 3)
     let items = schedule[day].filter(item => !(item.start == start && item.channel == channel))
@@ -59,8 +70,60 @@ async function RubmleRoutineAddToSchedule () {
   }
 }
 
+// mouse listeners
+function RumbleRoutineMouseDown (e) {
+  if (e.target.className !== 'rumble-routine-input') return
+  RumbleRoutineMouseEl = e.target
+  RumbleRoutineMouseStart = e.clientY
+  RumbleRoutineStartVal = RumbleRoutineMouseEl.value
+}
+function RubmleRoutineMouseMove (e) {
+  const el = RumbleRoutineMouseEl
+  if (!el) return
+  if (e.buttons == 0) return
+  e.preventDefault()
+  e.stopPropagation()
+  const diff = RumbleRoutineMouseStart - e.clientY
+  const increment = Math.floor(diff / 10)
+  const newVal = RumbleRoutineTimeAdd(RumbleRoutineStartVal, increment, el.dataset.pivot)
+  el.value = ('0' + newVal).slice(-2)
+}
+document.addEventListener('mousemove', RubmleRoutineMouseMove)
+document.addEventListener('mouseup', () => { RumbleRoutineMouseEl = null})
+
+// keypress listeners
+function RumbleRoutineKeyDown(e) {
+  const el = e.target
+  const pivot = el.dataset.pivot
+  e.preventDefault()
+  e.stopPropagation()
+  console.log(e.key, el.id)
+  if (e.key == 'ArrowUp') {
+    const num = RumbleRoutineTimeAdd(el.value, 1, pivot)
+    el.value = ('0' + num).slice(-2)
+  }
+  if (e.key == 'ArrowDown') {
+    const num = RumbleRoutineTimeAdd(el.value, -1, pivot)
+    el.value = ('0' + num).slice(-2)
+  }
+  if (e.key == 'ArrowRight' && el.id == 'rumbleroutine-hours')
+    document.getElementById('rumbleroutine-minutes').focus()
+  if (e.key == 'ArrowLeft' && el.id == 'rumbleroutine-minutes')
+    document.getElementById('rumbleroutine-hours').focus()
+}
+
 // close the bar
 function RubmleRoutineCloseBar () {
+  const hh = document.getElementById('rumbleroutine-hours')
+  if (hh) {
+    hh.removeEventListener('keypress', RumbleRoutineKeyDown)
+    hh.removeEventListener('mousedown', RumbleRoutineMouseDown)
+  }
+  const mm = document.getElementById('rumbleroutine-minutes')
+  if (mm) {
+    mm.removeEventListener('keypress', RumbleRoutineKeyDown)
+    mm.removeEventListener('mousedown', RumbleRoutineMouseDown)
+  }
   const el = document.getElementById('rumble-routine-bar')
   if (el) el.parentElement.removeChild(el)
 }
@@ -76,36 +139,41 @@ async function RubmleRoutineDisplayBar () {
   const div = document.createElement('div')
   el.appendChild(div)
 
-  const hours = document.createElement('select')
+  const timeNudge = document.createElement('div')
+  timeNudge.className = 'rumble-routine-time-nudge'
+
+  const hours = document.createElement('input')
   hours.id = 'rumbleroutine-hours'
-  hours.addEventListener('change', RumbleRoutineUpdateChecked)
-  Array.from({ length: 24 }, (_,i) => {
-    const option = document.createElement('option')
-    option.value = ('0' + i).slice(-2)
-    option.label = i
-    hours.appendChild(option)
-  })
-  hours.selectedIndex = dt.getHours()
-  div.appendChild(hours)
+  hours.className = 'rumble-routine-input'
+  hours.type = 'text'
+  hours.size = 2
+  hours.value = '00'
+  hours.dataset.pivot = 24
+  hours.addEventListener('mousedown', RumbleRoutineMouseDown)
+  hours.addEventListener('keydown', RumbleRoutineKeyDown)
+  hours.value = ('0' + dt.getHours()).slice(-2)
+  timeNudge.appendChild(hours)
 
   const colon = document.createElement(':')
   colon.innerHTML = ':'
-  div.appendChild(colon)
+  timeNudge.appendChild(colon)
 
-  const minutes = document.createElement('select')
+  const minutes = document.createElement('input')
   minutes.id = 'rumbleroutine-minutes'
-  minutes.addEventListener('change', RumbleRoutineUpdateChecked)
-  Array.prototype.forEach.call(['00', '30'], val => {
-    const option = document.createElement('option')
-    option.value = val
-    option.label = val
-    minutes.appendChild(option)
-  })
-  div.appendChild(minutes)
+  minutes.className = 'rumble-routine-input'
+  minutes.type = 'text'
+  minutes.size = 2
+  minutes.value = '00'
+  // minutes.value = ('0' + dt.getMinutes()).slice(-2)
+  minutes.dataset.pivot = 60
+  minutes.addEventListener('mousedown', RumbleRoutineMouseDown)
+  minutes.addEventListener('keydown', RumbleRoutineKeyDown)
+  timeNudge.appendChild(minutes)
+  div.appendChild(timeNudge)
 
   const sked = document.createElement('div')
   sked.className = 'inline-schedule'
-  dayNames.forEach(day => {
+  RumbleRoutineDayNames.forEach(day => {
     const name = day.substring(0, 3)
     const key = name.toLowerCase()
     const span = document.createElement('span')
@@ -132,25 +200,25 @@ async function RubmleRoutineDisplayBar () {
 
   RumbleRoutineUpdateChecked()
 
-  // const dd = dayNames[dt.getDay()].toLowerCase().substring(0, 3)
+  // const dd = RumbleRoutineDayNames[dt.getDay()].toLowerCase().substring(0, 3)
   // document.getElementById(`rumbleroutine-${dd}`).checked = true
 }
 
 async function RumbleRoutineUpdateChecked() {
   const hours = document.getElementById('rumbleroutine-hours').value
-  const minutes = document.getElementById('rumbleroutine-minutes').value
-  const start = `${hours}:${minutes}`
-
   const { channel } = RubmleRoutineGetChannel()
   const { schedule } = await chrome.storage.sync.get({ 
     schedule: { sun: [], mon: [], tue: [], wed: [], thu: [], fri: [], sat: [] }
   })
 
-  dayNames.forEach(dayName => {
+  RumbleRoutineDayNames.forEach(dayName => {
     const d = dayName.toLowerCase().substring(0, 3)
-    const present = schedule[d].find(item => item.channel == channel && item.start == start)
+    const startHH = document.getElementById('rumbleroutine-hours').value
+    const present = schedule[d].find(item => item.channel == channel && startHH == hours)
     if (present) {
+      const startMM = present.start.split(':')[1]
       document.getElementById(`rumbleroutine-${d}`).checked = true
+      document.getElementById('rumbleroutine-minutes').value = ('0' + startMM).slice(-2)
     }
   })
 }
